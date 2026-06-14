d40dbaad376bdb53e5cd4833a3bc98cd35bca737
Sakib hasan <sakibhasan16h@gmail.com>
2026-06-14 15:21:24 +0600

    inserted related data for each table

--- queries.sql
+++ queries.sql
@@ -3,8 +3,15 @@ CREATE TABLE users (
     full_name varchar(50),
     email varchar(100) unique,
     role varchar(20) check (role in ('Ticket Manager', 'Football Fan')),
-    phone_number int
+    phone_number varchar(30)
 );
+
+INSERT INTO users (user_id, full_name, email, role, phone_number) VALUES
+(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
+(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+
 
 CREATE TABLE matches (
     match_id serial primary key,
@@ -13,6 +20,14 @@ CREATE TABLE matches (
     base_ticket_price decimal(10, 2) check (base_ticket_price >= 0),
     match_status varchar(50) check (match_status in ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
 );
+
+
+INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status) VALUES
+(101, 'Real Madrid vs Barcelona', 'Champions League', 150.00, 'Available'),
+(102, 'Man City vs Liverpool', 'Premier League', 120.00, 'Selling Fast'),
+(103, 'Bayern Munich vs PSG', 'Champions League', 130.00, 'Available'),
+(104, 'AC Milan vs Inter Milan', 'Serie A', 90.00, 'Sold Out'),
+(105, 'Juventus vs Roma', 'Serie A', 80.00, 'Available');
 
 
 CREATE TABLE bookings (
@@ -25,8 +40,12 @@ CREATE TABLE bookings (
 );
 
 
-
-
+INSERT INTO bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost) VALUES
+(501, 1, 101, 'A-12', 'Confirmed', 150.00),
+(502, 1, 102, 'B-04', 'Confirmed', 120.00),
+(503, 2, 101, 'A-13', 'Confirmed', 150.00),
+(504, 2, 101, NULL, NULL, 150.00),
+(505, 3, 102, 'C-20', 'Pending', 120.00);
 
 
 
